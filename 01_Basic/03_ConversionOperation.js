7519d27018c1da3abeaad27832441458c1b7c8fc
PavanPawar12 <pavanpawar9923100@gmail.com>
2025-12-31 00:30:00 +0530

    Conversion of datatypes

---
+++ 01_Basic/03_ConversionOperation.js
@@ -0,0 +1,27 @@
+let score = "pavan"
+
+// console.log(typeof score)
+// console.log(typeof(score))
+
+let valueInNumber = Number(score)
+// console.log( typeof valueInNumber)
+// console.log(valueInNumber) 
+
+// "33" => 33
+// "33abc" => NaN
+// true => 1; false => 0
+
+
+let isLoggedIn = "pava"
+let booleanIsLoggesIn = Boolean(isLoggedIn)
+console.log(booleanIsLoggesIn)
+
+// 1 => true; 0 => false
+// "" => false
+// "pavan" => true
+
+let someNumber = 33;
+let stringNumber = String(someNumber)
+console.log(stringNumber);
+console.log(typeof(stringNumber))
+
