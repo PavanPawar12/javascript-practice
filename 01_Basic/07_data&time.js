a36323cd74d8899b70dcffbce388e7ca5a2ca9e7
PavanPawar12 <pavanpawar9923100@gmail.com>
2025-12-31 15:14:00 +0530

    time and dates

---
+++ 01_Basic/07_data&time.js
@@ -0,0 +1,33 @@
+const dates = new Date()
+// console.log(dates.toString())
+// console.log(dates.toISOString())
+// console.log(dates.toLocaleString())
+// console.log(typeof dates)
+
+
+// let myCreatedDates = new Date(2023, 0, 4)
+// let myCreatedDates = new Date(2023, 0, 4,5,3) // 4/1/2023, 5:03:00 am
+let myCreatedDates = new Date("2023-01-02") // 2/1/2023, 5:30:00 am
+// console.log(myCreatedDates.toLocaleString())
+
+
+let myTimeStamp = Date.now()
+// console.log(myTimeStamp) //milisecond
+// console.log(myCreatedDates.getTime()) 
+// console.log(Math.floor(Date.now()/1000))
+
+
+/**
+let now = new Date();
+
+console.log(now.getFullYear()); // Year
+console.log(now.getMonth());    // Month (0–11)
+console.log(now.getDate());     // Day of month
+console.log(now.getDay());      // Day of week (0–6)
+
+console.log(now.getHours());    // Hours
+console.log(now.getMinutes());  // Minutes
+console.log(now.getSeconds());  // Seconds
+console.log(now.getMilliseconds()); 
+ 
+*/
